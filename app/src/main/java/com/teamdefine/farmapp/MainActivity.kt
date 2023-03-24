2979c8b5e47cdd43277d07990ce7671fcf6a6fe3
Nitish Sharma <nitish.sharma1186@gmail.com>
2023-03-24 10:04:50 +0530

    socket hata diya, ache se krunga ferse

--- app/src/main/java/com/teamdefine/farmapp/MainActivity.kt
+++ app/src/main/java/com/teamdefine/farmapp/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import io.socket.client.Socket
 
 class MainActivity : AppCompatActivity() {
-    var socketIOInstance: Socket? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
