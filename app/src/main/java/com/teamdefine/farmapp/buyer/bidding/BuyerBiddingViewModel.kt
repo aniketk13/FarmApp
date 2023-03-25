efc38bc1ab4d0c5644fa63d107ee3fc0d10ac4eb
Nitish Sharma <nitish.sharma1186@gmail.com>
2023-03-25 18:19:26 +0530

    starting from scratch, please don't kill me @aniketk13

--- app/src/main/java/com/teamdefine/farmapp/buyer/bidding/BuyerBiddingViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.teamdefine.farmapp.buyer.bidding
-
-import androidx.lifecycle.ViewModel
-
-class BuyerBiddingViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/teamdefine/farmapp/farmer/newItem/FarmerNewItemViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.teamdefine.farmapp.farmer.newItem
-
-import androidx.lifecycle.ViewModel
-
-class FarmerNewItemViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
