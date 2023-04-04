f1c3d2671c88484ab502f1d1d8d46719108569de
aniketk13 <ani.khajanchi257@gmail.com>
2023-04-04 16:26:30 +0530

    bidding screen generated

---
+++ app/src/main/java/com/teamdefine/farmapp/buyer/bidding/BuyerBidding.kt
@@ -0,0 +1,22 @@
+package com.teamdefine.farmapp.buyer.bidding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.teamdefine.farmapp.databinding.FragmentBuyerBiddingBinding
+
+class BuyerBidding : Fragment() {
+
+    private lateinit var binding: FragmentBuyerBiddingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = FragmentBuyerBiddingBinding.inflate(inflater, container, false).also {
+        binding = it
+    }.root
+
+
+}
---
+++ app/src/main/java/com/teamdefine/farmapp/buyer/bidding/BuyerBiddingVM.kt
@@ -0,0 +1,7 @@
+package com.teamdefine.farmapp.buyer.bidding
+
+import androidx.lifecycle.ViewModel
+
+class BuyerBiddingVM : ViewModel() {
+    // TODO: Implement the ViewModel
+}
