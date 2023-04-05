85263238d24813fbaf51b7a2972e4570e263b85d
aniketk13 <ani.khajanchi257@gmail.com>
2023-04-05 23:04:09 +0530

    farmer crop bids screen in making

--- app/src/main/java/com/teamdefine/farmapp/farmer/cropBids/FarmerCropBids.kt
+++ app/src/main/java/com/teamdefine/farmapp/farmer/cropBids/FarmerCropBids.kt
@@ -1,15 +1,22 @@
 package com.teamdefine.farmapp.farmer.cropBids
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.teamdefine.farmapp.app.utils.Utility.loadImageUsingGlide
+import com.teamdefine.farmapp.buyer.bidding.models.BiddingData
 import com.teamdefine.farmapp.databinding.FragmentFarmerCropBidsBinding
+import com.teamdefine.farmapp.farmer.home.FarmerHomeScreenAdapter
+import com.teamdefine.farmapp.farmer.models.FarmerCrops
 
 class FarmerCropBids : Fragment() {
 
@@ -19,6 +26,7 @@ class FarmerCropBids : Fragment() {
     private var cropId: String? = null
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var database: FirebaseFirestore
+    private var adapter: RecyclerView.Adapter<FarmerCropBidsAdapter.ViewHolder>? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,15 +48,56 @@ class FarmerCropBids : Fragment() {
     }
 
     private fun initViews() {
-        showCropDetails()
+        getCrop()
+        getBids()
     }
 
-    private fun showCropDetails() {
+    private fun getBids() {
+        viewModel.getBids(database,cropId)
+    }
+
+    private fun getCrop() {
         viewModel.getCrop(database,cropId)
     }
 
     private fun initObservers() {
-        TODO("Not yet implemented")
+        viewModel.crop.observe(requireActivity()){
+            it?.let {
+                showCropDetails(it)
+            }
+        }
+        viewModel.cropBids.observe(requireActivity()){
+            it?.let {
+                setupDataInRecyclerView(it)
+            }
+        }
+    }
+
+    private fun setupDataInRecyclerView(cropBids: ArrayList<BiddingData>) {
+        adapter = activity?.let {
+            FarmerCropBidsAdapter(
+                it,
+                cropBids,
+                object : FarmerCropBidsAdapter.ItemClickListener {
+                    override fun onItemClickListener(clickedCropBid: BiddingData) {
+                        Log.i("FarmerCropBids", clickedCropBid.CropId.toString())
+                    }
+                })
+        }
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(activity)
+//        if (binding.swipeRefresh.isRefreshing)
+//            binding.swipeRefresh.isRefreshing = false
+    }
+
+    private fun showCropDetails(crop: FarmerCrops) {
+        binding.apply {
+            cropNameTv.text = crop.CropName
+            requireContext().loadImageUsingGlide(crop.CropImageURI.toString(), cropIv)
+            farmerNameTv.text=crop.FarmerName
+            dateTv.text=crop.CropListingDate
+            priceOfCrop.text=crop.CropOfferPrice.toString()
+        }
     }
 
     private fun initClickListeners() {
--- app/src/main/java/com/teamdefine/farmapp/farmer/cropBids/FarmerCropBidsVM.kt
+++ app/src/main/java/com/teamdefine/farmapp/farmer/cropBids/FarmerCropBidsVM.kt
@@ -1,14 +1,34 @@
 package com.teamdefine.farmapp.farmer.cropBids
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.FirebaseFirestore
+import com.teamdefine.farmapp.buyer.bidding.models.BiddingData
+import com.teamdefine.farmapp.farmer.models.FarmerCrops
 
 class FarmerCropBidsVM : ViewModel() {
+    private val _cropBids: MutableLiveData<ArrayList<BiddingData>> = MutableLiveData(null)
+    val cropBids: LiveData<ArrayList<BiddingData>>
+        get() = _cropBids
+
+    private val _crop: MutableLiveData<FarmerCrops> = MutableLiveData(null)
+    val crop: LiveData<FarmerCrops>
+        get() = _crop
+
     fun getCrop(database:FirebaseFirestore,cropId: String?) {
+        database.collection("Crops").document(cropId!!).get()
+            .addOnCompleteListener {
+                _crop.postValue(it.result.toObject(FarmerCrops::class.java))
+            }
+    }
+
+    fun getBids(database: FirebaseFirestore, cropId: String?) {
+        var bids:ArrayList<BiddingData>?= arrayListOf()
         database.collection("Bidding").whereEqualTo("CropId",cropId).get()
             .addOnCompleteListener {
                 for(document in it.result){
-
+                    bids?.add(document.toObject(BiddingData::class.java))
                 }
             }
     }
