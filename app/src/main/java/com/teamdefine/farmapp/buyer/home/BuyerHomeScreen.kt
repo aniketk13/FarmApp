103e51670c309e4c762071f28914ce921c123648
aniketk13 <ani.khajanchi257@gmail.com>
2023-04-05 11:48:38 +0530

    signout implemented in buyer screen

--- app/src/main/java/com/teamdefine/farmapp/buyer/home/BuyerHomeScreen.kt
+++ app/src/main/java/com/teamdefine/farmapp/buyer/home/BuyerHomeScreen.kt
@@ -8,7 +8,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -18,7 +17,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.teamdefine.farmapp.MainActivity
 import com.teamdefine.farmapp.app.utils.Utility.toast
-import com.teamdefine.farmapp.buyer.MainBuyerActivity
 import com.teamdefine.farmapp.databinding.FragmentBuyerHomeScreenBinding
 import com.teamdefine.farmapp.farmer.models.FarmerCrops
 
